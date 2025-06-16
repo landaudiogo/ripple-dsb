2b2474da03ce69ed6c2e468ad840d777e2b3defe
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-06-16 08:36:11 +0200

    Media ms local docker experiment

--- mediaMicroservices/scripts/compose_review.sh
+++ mediaMicroservices/scripts/compose_review.sh
@@ -6,6 +6,6 @@ for i in {1..10}; do
   title="title_"$i
   text=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 256 | head -n 1)
   curl -d "text="$text"&username="$username"&password="$password"&rating=5&title="$title \
-      http://127.0.0.1:8080/wrk2-api/review/compose
+      http://18.193.68.182:3009/wrk2-api/review/compose
 done
  
--- mediaMicroservices/scripts/register_movies.sh
+++ mediaMicroservices/scripts/register_movies.sh
@@ -3,5 +3,5 @@
 for i in {1..1000}; do
   echo "Movie $i"
   curl -d "title=title_"$i"&movie_id=movie_id_"$i \
-      http://127.0.0.1:8080/wrk2-api/movie/register
+      http://18.193.68.182:3009/wrk2-api/movie/register
 done
