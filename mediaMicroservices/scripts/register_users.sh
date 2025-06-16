967f45ede51554e38658bc8244644724a626b6d0
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-06-16 20:25:10 +0200

    Add media remote setup

--- mediaMicroservices/scripts/register_users.sh
+++ mediaMicroservices/scripts/register_users.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in {5..1000}; do
+for i in {1..1000}; do
   echo "User $i"
   curl -d "first_name=first_name_"$i"&last_name=last_name_"$i"&username=username_"$i"&password=password_"$i \
       http://18.193.68.182:3009/wrk2-api/user/register
